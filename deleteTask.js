dc26b55f378f918c8b011e5ab5a84026ee0c6ef4
JC-GIRALDO-GIRALDO <kmilom19@gmail.com>
2023-08-28 21:07:39 -0500

    modifique el review

--- deleteTask.js
+++ deleteTask.js
@@ -1,9 +1,12 @@
 module.exports = function deleteTask(tasks, rl) {
   return new Promise((resolve) => {
+    console.log("..........................");
+    console.log("Eliminando tarea de la lista");
     setTimeout(() => {
       rl.question("Índice de la tarea a eliminar: ", (index) => {
         if (index >= 0 && index < tasks.length) {
           tasks.splice(index, 1);
+          console.log("..........................");
           console.log("Tarea eliminada.");
         } else {
           console.log("Índice inválido.");
